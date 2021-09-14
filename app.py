c883bf840e1b5ec289c9bf1aa60e3ccf3dca70de
luomein <luomein@gmail.com>
2021-09-14 10:14:07 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -8,32 +8,65 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+
+
 st.set_page_config(layout="wide")
-st.markdown(''' <style> div.stButton > button:first-child {
-font-size:20px;height:3em;width:15em;border-radius:10px 10px 10px 10px;border: 0px;
-} </style>
+st.markdown('''
+<style>
+        div.stSelectbox > label {
+        font-size: 20px;font-family: "IBM Plex Sans", sans-serif;font-weight: 600;
+        }
+ </style>
 ''', unsafe_allow_html=True)
 ### background-color: #00cc00;
 ###### div.stButton > button:first-child {{ border: 5px solid {primaryColor}; border-radius:20px 20px 20px 20px; }}
 
 def get_Complication(df , family) :
-    return df.loc[df['ComplicationFamily'] == family ].reset_index()
+    return df.loc[df['ComplicationFamily_Name'] == family ].reset_index()
 
-def get_ComplicationFamily(df , family) :
-    return df.loc[df['ComplicationFamily'] == family ].reset_index().iloc[0]
-def get_WatchFace(df , family) :
-    return df.loc[df['ComplicationFamily'] == family ].reset_index()
+def get_ComplicationFamily(df , family ) :
+       return df.loc[df['ComplicationFamily_Name'] == family ].reset_index().iloc[0]
+
+def get_WatchFace(df , family, watchface = '') :
+  #st.write(type(watchface))
+  if watchface == '' :
+    return df.loc[df['ComplicationFamily_Name'] == family ].reset_index()
+  else :
+    return df.loc[(df['ComplicationFamily_Name'] == family) & (df['WatchFace_Name'] == watchface)  ].reset_index()
 
 ComplicationTemplate = pd.read_csv('ComplicationTemplate.csv')
-ComplicationTemplate.rename(columns={"Name":"ComplicationTemplate" , "Name.1":"ComplicationFamily"} , inplace=True)
+ComplicationTemplate.rename(columns={"Name":"ComplicationTemplate_Name" , "Name.1":"ComplicationFamily_Name"} , inplace=True)
 ComplicationTemplate['Picture_URL'].fillna('', inplace = True)
 
 ##CLKComplicationFamily,Name,Picture_URL,URL,Name,Picture_URL
 WatchFace = pd.read_csv('WatchFace.csv')
-WatchFace.rename(columns={"Name":"ComplicationFamily" , "Name.1":"WatchFace"
+WatchFace.rename(columns={"Name":"ComplicationFamily_Name" , "Name.1":"WatchFace_Name"
   , "Picture_URL" : "ComplicationFamily_Picture_URL", "URL" : "ComplicationFamily_URL" , "Picture_URL.1" : "WatchFace_Picture_URL"} , inplace=True)
 #WatchFace['Picture_URL'].fillna('', inplace = True)
 #all_checked = st.sidebar.checkbox('(All)' , value = True)
+#st.sidebar.header('WatchFace')
+st.markdown('''
+<style>
+    section.main div.stButton > button:first-child {
+                    font-size:30px;border: 0px;
+                    position: fixed;
+                    top: -20px;
+                    left:  -180px;
+                    z-index : 10000;
+                    outline:none;
+                    background-color: Transparent;
+                    }
+ </style>
+''', unsafe_allow_html=True)
+home_btn = st.button("🏚")
+
+if home_btn and 'selected_menu'   in st.session_state :
+      st.session_state['selected_menu'] = ''
+
+selected_watch_face =  st.sidebar.selectbox('WatchFace' , ('' , ) + (*(WatchFace.WatchFace_Name.unique().tolist()),)   )
+#st.sidebar.write(selector_watch_face)
+#st.sidebar.markdown(f"<hr>"  , unsafe_allow_html=True )
+
 st.sidebar.header('ComplicationTemplate components')
 
 filter = dict()
@@ -44,18 +77,25 @@ filter['CLKGauageProvider'] = st.sidebar.checkbox('CLKGauageProvider', value = T
 filter['CLKComplicationRingStyle'] = st.sidebar.checkbox('CLKComplicationRingStyle', value = True)
 
 #def save_menu_selected() :
-
-
+#st.sidebar.markdown(f"<hr>"  , unsafe_allow_html=True )
+st.markdown('''
+<style>
+        div.stButton > button:first-child {
+                    font-size:20px;height:3em;width:15em;border-radius:10px 10px 10px 10px;border: 0px;
+                    }
+ </style>
+''', unsafe_allow_html=True)
 st.sidebar.header('ComplicationFamily')
 side_menu = dict()
-for f in ComplicationTemplate['ComplicationFamily'].unique() :
+for f in sorted( ComplicationTemplate['ComplicationFamily_Name'].dropna().unique() ) :
     sub_df = get_Complication(ComplicationTemplate , f)
+    filtered_watch_face = get_WatchFace(WatchFace , f , watchface = selected_watch_face)
     has_filtered_element = False
     for ele in filter :
         if filter[ele] and  sub_df['Components'].str.contains(ele).any() :
             has_filtered_element = True
             break
-    if has_filtered_element:
+    if has_filtered_element and  len(filtered_watch_face) > 0  :
       side_menu[f] = st.sidebar.button(f)
       if side_menu[f] :
            st.session_state['selected_menu'] = f
@@ -86,7 +126,7 @@ for f in side_menu :
         for i in range(len(sub_watch_face) ) :
             #watchface_cols[i+1].write(sub_watch_face.loc[i , 'WatchFace_Picture_URL'])
             watchface_cols[i+1].image(sub_watch_face.loc[i , 'WatchFace_Picture_URL'] , use_column_width = 'auto')
-            watchface_cols[i+1].write(sub_watch_face.loc[i , 'WatchFace'])
+            watchface_cols[i+1].write(sub_watch_face.loc[i , 'WatchFace_Name'])
         #st.markdown(f"#[{f}]({get_ComplicationFamily(WatchFace , f)['ComplicationFamily_URL']})#"  , unsafe_allow_html=True )
         st.markdown(f"<br><br><br><br><br>"  , unsafe_allow_html=True )
         #st.header('ComplicationTemplate')
@@ -107,7 +147,7 @@ for f in side_menu :
         for idx , row in df.iterrows():
            dt_cols = st.columns([3] + [1] * len(checked_filter)  )
            #dt_cols[0].write(row['ComplicationTemplate'])
-           with dt_cols[0].expander(row['ComplicationTemplate']) :
+           with dt_cols[0].expander(row['ComplicationTemplate_Name']) :
                #st.markdown(f"[{row['URL']}]({row['URL']})", unsafe_allow_html=True)
                st.markdown(f"<a href='{row['URL']}' target='_blank'><img src='{row['Picture_URL']}'></a>", unsafe_allow_html=True)
                #if row['Picture_URL'] != np.nan and  row['Picture_URL']!= '' :
@@ -120,4 +160,36 @@ for f in side_menu :
         #dt_cols[0].write( df[['ComplicationTemplate'] + checked_filter ].replace(['True','False'],['✓','']).to_html(escape=False , index=False)  , unsafe_allow_html=True  )
         #dt_cols[1].write('test')
         #streamlit.expander(label, expanded=False)
-        
+st.markdown('''
+<style>
+        table.dataframe  thead {
+        vertical-align: bottom;
+        }
+ </style>
+''', unsafe_allow_html=True)
+
+if  ('selected_menu' not  in st.session_state ) or ('selected_menu'   in st.session_state  and st.session_state['selected_menu'] == '') :
+        ##df.pivot(index='foo', columns='bar', values='baz')
+
+        WatchFace['value'] = '✓'
+        WatchFace.fillna( '' , inplace = True)
+        WatchFace['WatchFace'] =  WatchFace['WatchFace_Name'] + '<br><img  width="80" src="' + WatchFace['WatchFace_Picture_URL'] + '" >'
+        if selected_watch_face != '' :
+            WatchFace = WatchFace[WatchFace['WatchFace_Name'] == selected_watch_face ]
+        WatchFace_pivot = WatchFace.pivot(index='WatchFace', columns='ComplicationFamily_Name', values='value' )
+        WatchFace_pivot.fillna( '' , inplace = True)
+        WatchFace_pivot = WatchFace_pivot.T.reset_index()
+
+        ComplicationTemplate['has_filtered_element'] = 0
+        for ele in filter :
+            if filter[ele]  :
+                ComplicationTemplate['has_filtered_element'] = ComplicationTemplate['has_filtered_element'] +  ComplicationTemplate['Components'].str.contains(ele , na = False ).astype('int32')
+        #df[df['A'].isin([3, 6])]
+        WatchFace_pivot = WatchFace_pivot[WatchFace_pivot['ComplicationFamily_Name'].isin(ComplicationTemplate.loc[ ComplicationTemplate['has_filtered_element'] > 0 , 'ComplicationFamily_Name'].dropna().unique().tolist()  ) ]
+        WatchFace_pivot.rename(columns={"ComplicationFamily_Name":" "  } , inplace = True)
+        #WatchFace_pivot.drop(columns=['WatchFace'] , inplace = True)
+        #df.drop(columns=['B', 'C'])
+        #WatchFace_pivot.replace('<NA>' , '' , inplace = True)
+        #st.dataframe(WatchFace_pivot)
+        st.title('Apple Watch Complication Lookbook')
+        st.write(WatchFace_pivot.to_html(escape=False , index=False , index_names = False ) , unsafe_allow_html=True )
